790860fe19a92b232b85cef398a90262f27c8126
AlexJerabek <aljerabe@microsoft.com>
2022-10-05 13:42:55 -0700

    Change links from docs.microsoft.com to learn.microsoft.com

--- types/office-runtime/index.d.ts
+++ types/office-runtime/index.d.ts
@@ -37,7 +37,7 @@ declare namespace OfficeRuntime {
    * [Api set: SharedRuntime 1.1, Mailbox 1.10]
    *
    * **Important**: In Outlook, support is only available with
-   * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+   * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
    *
    * Storage limit is 10 MB per domain, which may be shared by multiple add-ins.
    */
@@ -50,7 +50,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      *
      * @param key Key of item to be retrieved. Must be a string.
      */
@@ -63,7 +63,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      *
      * @param key Key of item to be set. Must be a string.
      * @param value Must be a string.
@@ -77,7 +77,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      *
      * @param key Key of item to be removed. Must be a string.
      */
@@ -90,7 +90,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      *
      * @param keys Keys of items to be removed. Must be an array of strings.
      */
@@ -103,7 +103,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      *
      * @param keyValues Key-value pairs to be set. Must be strings.
      */
@@ -116,7 +116,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      *
      * @param keys Keys of items to be removed. Must be an array of strings.
      */
@@ -129,7 +129,7 @@ declare namespace OfficeRuntime {
      * [Api set: SharedRuntime 1.1, Mailbox 1.10]
      *
      * **Important**: In Outlook, support is only available with
-     * {@link https://docs.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
+     * {@link https://learn.microsoft.com/office/dev/add-ins/outlook/autolaunch | event-based activation}.
      */
     getKeys(): Promise<string[]>;
   }
@@ -255,7 +255,7 @@ declare namespace OfficeRuntime {
    *
    * @remarks
    *
-   * The methods in this interface are equivalent to those in the {@link https://docs.microsoft.com/javascript/api/office/office.auth | Office.auth interface}.
+   * The methods in this interface are equivalent to those in the {@link https://learn.microsoft.com/javascript/api/office/office.auth | Office.auth interface}.
    * If new authentication types are added in the future, they will only be added to the `Office.auth` interface.
    * For simplicity, the code examples throughout the documentation use `Office.auth`.
    */
@@ -263,7 +263,7 @@ declare namespace OfficeRuntime {
     /**
      * Calls the Azure Active Directory V 2.0 endpoint to get an access token to your add-in's web application. Enables add-ins to identify users.
      * Server-side code can use this token to access Microsoft Graph for the add-in's web application by using the
-     * {@link https://docs.microsoft.com/azure/active-directory/develop/active-directory-v2-protocols-oauth-on-behalf-of | "on behalf of" OAuth flow}.
+     * {@link https://learn.microsoft.com/azure/active-directory/develop/active-directory-v2-protocols-oauth-on-behalf-of | "on behalf of" OAuth flow}.
      * This API requires a single sign-on configuration that bridges the add-in to an Azure application. Office users sign-in with Organizational
      * Accounts and Microsoft Accounts. Microsoft Azure returns tokens intended for both user account types to access resources in the Microsoft Graph.
      *
